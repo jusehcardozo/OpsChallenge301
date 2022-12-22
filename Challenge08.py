10cd139b52b477e39a3c9b09f5c9e789d6c5fbd8
Jose Cardozo <116588590+jusehcardozo@users.noreply.github.com>
2022-12-21 16:28:09 -0800

    Create Challenge08.py

---
+++ Challenge08.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Challenge 08
+# Author: Jose Cardozo
+# Date of latest version:  12/21/2022
+# Purpose: Use lists and sets.
+
+# I used this link to get instructions on how to do this:
+# https://github.com/codefellows/seattle-ops-301d7/blob/main/class-09/challenges/DEMO.md
+
+# Create a Python script that includes the following:
+
+# Assign to a variable a list of ten string elements.
+fruitlist = ["blue", "orange", "green", "red", "black", "pink", "yellow", "white", "brown", "purple"]
+print(fruitlist)
+
+# Print the fourth element of the list.
+
+fruitlist = ["blue", "orange", "green", "red", "black", "pink", "yellow", "white", "brown", "purple"]
+print(fruitlist[3])
+
+# Print the sixth through tenth element of the list.
+
+fruitlist = ["blue", "orange", "green", "red", "black", "pink", "yellow", "white", "brown", "purple"]
+print(fruitlist[5:9])
+
+# Change the value of the seventh element to "onion".
+
+fruitlist[6] = "onion"
+print(fruitlist)
+
+# yeeehaw!
