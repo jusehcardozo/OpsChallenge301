cf77493ab9bf39ac4313b5490f2b489251f32699
Jose Cardozo <116588590+jusehcardozo@users.noreply.github.com>
2022-12-30 11:26:18 -0800

    Update Challenge07.sh

--- Challenge07.sh
+++ Challenge07.sh
@@ -3,15 +3,15 @@
 # Import libraries
 
 # Script must use the os.walk() function from the library.
-Import os
+import os
 
 
 # Script must ask the user for a file path and user input string into a variable.
 dir = input("Enter a directory name:")
 
 # Script must use the os.walk() function within a python function that hands in the user input file path.
-def os_walk_target(target_arg):
-    for (root, dirs, files) in os.walk("testdir"):
+def directoryprint(dir):
+    for (root, dirs, files) in os.walk(dir):
         ### Add a print command here to print ==root==
         print(root)
         ### Add a print command here to print ==dirs==
@@ -19,6 +19,6 @@ def os_walk_target(target_arg):
         ### Add a print command here to print ==files==
         print(files)
 
-os_walk_target(dir)
+directoryprint(dir)
 
 # End
